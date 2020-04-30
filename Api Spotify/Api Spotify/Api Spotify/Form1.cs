c63d2136411a03b93d2571d78c281cbc74b96a46
Pablond420 <64156988+Pablond420@users.noreply.github.com>
2020-04-30 18:09:38 -0500

    full get

--- Api Spotify/Api Spotify/Api Spotify/Form1.cs
+++ Api Spotify/Api Spotify/Api Spotify/Form1.cs
@@ -16,5 +16,10 @@ namespace Api_Spotify
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
