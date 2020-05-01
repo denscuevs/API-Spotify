666080b09d779783d7c5a53871fdeaba3ab3e740
Pablond420 <64156988+Pablond420@users.noreply.github.com>
2020-04-30 21:25:23 -0500

    Tqm DMIB

--- Api Spotify/Api Spotify/Api Spotify/Form1.Designer.cs
+++ Api Spotify/Api Spotify/Api Spotify/Form1.Designer.cs
@@ -29,33 +29,58 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(500, 83);
+            this.button1.Location = new System.Drawing.Point(33, 68);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(182, 117);
+            this.button1.Size = new System.Drawing.Size(155, 55);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "GET";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 129);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(155, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "POST";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 635);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(702, 676);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Api Spotify/Api Spotify/Api Spotify/Form1.cs
+++ Api Spotify/Api Spotify/Api Spotify/Form1.cs
@@ -8,18 +8,102 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+using System.Net;
+
 namespace Api_Spotify
 {
     public partial class Form1 : Form
     {
+        string playlist_url;
+        string token;
         public Form1()
         {
             InitializeComponent();
+            playlist_url = "https://api.spotify.com/v1/tracks/7f0vVL3xi4i78Rv5Ptn2s1";
+            token = "Bearer BQBRQcZYLDlcJRWslqb6Iiny4_2cnVKYAiNEzfzJ3tynbUnbxeeK4h9Gbm7BwcR" +
+                "4ApT80qLWl4O9Ighz1TMUOnrJ7dCS8TNS3No-7YFQjXaQV2AEdFLfvc0iRkm_9U-vqc9GYr8Nh1Vhl" +
+                "IhcEQWHkZVVIGeC-UQXTDnr7df6HPY6sbL0QOCRvI0JiU-ye_GykkdnSxaupj-6KfmrA8e67NoMQIW" +
+                "xrs_QtkEWkRtl1ZTjtqTSCw67Yc8VlZOT3UT7K4mGPCMQ0hA6ddHQ5FnerMnfjMGgOEj3";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            // Crear la peticion a la URL. 
+
+            WebRequest request = WebRequest.Create(playlist_url);
+
+            request.Headers.Add("Authorization", token);
+
+            //credenciales en caso de ser necesarias (se colocan default en caso de que no)
+            //request.Credentials = CredentialCache.DefaultCredentials;
+
+            // obtener la respuesta
+            WebResponse response = request.GetResponse();
+
+            // Mostrar el status
+            textBox1.Text = ((HttpWebResponse)response).StatusDescription;
+
+            // obtener la informacion que regreso la peticion 
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // crear un lector
+                StreamReader reader = new StreamReader(dataStream);
+                // leer el contenido
+                string responseFromServer = reader.ReadToEnd();
+                // mostrar el contenido
+                textBox1.Text += responseFromServer;
+            }
+
+            // cerrar respuesta
+            response.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a request using a URL that can receive a post.
+            WebRequest request = WebRequest.Create("http://www.contoso.com/PostAccepter.aspx ");
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+
+            // Create POST data and convert it to a byte array.
+            string postData = "This is a test that posts this string to a Web server.";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+
+            textBox1.Text = ((HttpWebResponse)response).StatusDescription;
+            // Get the stream containing content returned by the server.
+            // The using block ensures the stream is automatically closed.
+            using (dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                textBox1.Text += responseFromServer;
+            }
+
+
+            // Close the response.
+            response.Close();
+
         }
     }
 }
